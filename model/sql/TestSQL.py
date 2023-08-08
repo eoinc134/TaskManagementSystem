273f50393216e392321659380fe3bdb48f746b76
eoinc134 <60822259+eoinc134@users.noreply.github.com>
2023-08-08 21:24:00 +0100

    Retrieving user table from database

--- model/sql/TestSQL.py
+++ model/sql/TestSQL.py
@@ -2,3 +2,7 @@ from Sql import Sql
 
 sql = Sql()
 cursor = sql.cnxn.cursor()
+
+cursor.execute("SELECT TOP (1000) [UserID],[Email],[Password] FROM [TaskManagementSystem].[dbo].[Users]")
+for row in cursor:
+    print('row = %r' % (row,))

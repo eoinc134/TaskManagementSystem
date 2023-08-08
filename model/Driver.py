73326d62c8387c0da19074402341f21a429c1070
eoinc134 <60822259+eoinc134@users.noreply.github.com>
2023-08-08 22:40:49 +0100

    restructure

--- model/Driver.py
+++ model/Driver.py
@@ -1,8 +1,10 @@
-from Sql import Sql
+from sql.Sql import Sql
 
-sql = Sql()
-cursor = sql.cnxn.cursor()
+database = Sql()
+cursor = database.cnxn.cursor()
 
 cursor.execute("SELECT TOP (1000) [UserID],[Email],[Password] FROM [TaskManagementSystem].[dbo].[Users]")
+
 for row in cursor:
+    
     print('row = %r' % (row,))

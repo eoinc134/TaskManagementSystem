c00b07037afee17defb7be11012e150b398176dd
eoinc134 <60822259+eoinc134@users.noreply.github.com>
2023-08-15 17:28:18 +0100

    beginning of login/registration functionality

--- routes/app.py
+++ routes/app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template
-import pandas as pd
+from flask import Flask, render_template, session
 
 from ..model.sql.Sql import Sql
 
@@ -14,6 +13,27 @@ def home_page():
 
     return render_template('home_page.html', lists=lists)
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/validateRegistration')
+def validateRegistration():
+    return render_template('home_page.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/validateLogin')
+def validateLogin():
+    session['login'] = True
+    return render_template('home_page.html', data=session['login'])
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return render_template('home_page.html')
 
 def get_db_connection():
     database = Sql()

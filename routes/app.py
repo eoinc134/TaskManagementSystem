2531b92fcc3c9cb89967477c44f8f3e20d1238db
eoinc134 <60822259+eoinc134@users.noreply.github.com>
2023-08-15 16:56:42 +0100

    page now isplays to do lists

--- routes/app.py
+++ routes/app.py
@@ -1,7 +1,20 @@
 from flask import Flask, render_template
+import pandas as pd
+
+from ..model.sql.Sql import Sql
 
 app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return render_template('home_page.html')
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    lists = cursor.execute("SELECT * FROM Lists").fetchall()
+
+    return render_template('home_page.html', lists=lists)
+
+
+def get_db_connection():
+    database = Sql()
+    return database.cnxn
